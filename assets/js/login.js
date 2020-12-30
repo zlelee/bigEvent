26d4edeaa93ffbbf85f18c943d28cadb9a4d6c49
lele <562282501@qq.com>
2020-12-30 15:29:12 +0800

    表单验证

--- assets/js/login.js
+++ assets/js/login.js
@@ -7,4 +7,15 @@ $(function () {
     $('.login-box').show()
     $('.reg-box').hide()
   })
+  const form = layui.form
+  form.verify({
+    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+    repwd: function (value) {
+      let pwd = $('.reg-box [name=repassword]').val()
+      console.log(pwd)
+      if (value !== pwd) {
+        return '两次密码不一致'
+      }
+    }
+  })
 })

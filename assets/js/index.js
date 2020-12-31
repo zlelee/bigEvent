6b8547c5936c769b85ee354e425ea007f17ba310
lele <562282501@qq.com>
2020-12-31 15:42:06 +0800

    没登陆禁止跳转后台

--- assets/js/index.js
+++ assets/js/index.js
@@ -24,13 +24,14 @@ function getUserInfo() {
 }
 function renderUserinfo(data) {
   let name = data.nickname || data.username
-  let first = name[0].toUpperCase()
+
   if (data.user_pic !== null) {
     $('.welcome').html('欢迎' + name)
     $('.layui-nav-img').attr('src', data.user_pic).show()
     $('.text-avatar').hide()
   } else {
     $('.welcome').html('欢迎' + name)
+    let first = name[0].toUpperCase()
     $('.layui-nav-img').hide()
     $('.text-avatar').html(first).show()
   }
